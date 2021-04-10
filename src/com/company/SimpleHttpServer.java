739f63bf0f3d097b9be6bff6a7fc2fd553993618
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-10 19:14:35 +0600

    Cleaned up more.

--- src/com/company/SimpleHttpServer.java
+++ src/com/company/SimpleHttpServer.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -11,31 +10,31 @@ import java.net.Socket;
  * Concept from: http://www.java2s.com/Code/Java/Network-Protocol/AverysimpleWebserverWhenitreceivesaHTTPrequestitsendstherequestbackasthereply.htm
  */
 public class SimpleHttpServer {
-	public static void main (String[] args) throws IOException {
+	public static void main (String[] args) {
 		try {
 			int port = 8080;
 			ServerSocket serverSocket = new ServerSocket(port);
 
 			while (true) {
-				// Wait for a client to connect. The method will block;
-				// when it returns the socket will be connected to the client
-				Socket client = serverSocket.accept();
+				System.out.println("Waiting for a request...");
+				Socket socket = serverSocket.accept();
+				System.out.println("Request received.");
 
-				// Get input and output streams to talk to the client
-				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				PrintWriter out = new PrintWriter(client.getOutputStream());
+				String path = "index.html";
 
-				String httpResponse = new HttpResponse().getResponse("index.html");
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+				String httpResponse = new HttpResponse().getResponse(path);
 				out.print(httpResponse);
 
-				out.close(); // Flush and close the output stream
-				in.close(); // Close the input stream
-				client.close(); // Close the socket itself
-			} // Now loop again, waiting for the next connection
+				out.close();
+				in.close();
+				socket.close();
+			}
 		}
-		catch (Exception e) {
-			System.err.println(e);
-			System.err.println("Usage: java HttpMirror <port>");
+		catch (Exception ex) {
+			System.out.println(ex);
 		}
 	}
 }
