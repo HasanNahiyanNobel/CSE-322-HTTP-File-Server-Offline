cf18c349d85334fd5b1fc746d14f8d3fb3b0a67a
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-10 12:53:59 +0600

    Implemented basic server-client.

---
+++ src/com/company/Server.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main (String[] args) throws IOException, ClassNotFoundException {
+		ServerSocket welcomeSocket = new ServerSocket(6666);
+
+		while (true) {
+			System.out.println("Waiting for connection...");
+			Socket socket = welcomeSocket.accept();
+			System.out.println("Connection established.");
+
+			// Open thread
+			Thread worker = new Worker(socket);
+			worker.start();
+		}
+
+	}
+}
