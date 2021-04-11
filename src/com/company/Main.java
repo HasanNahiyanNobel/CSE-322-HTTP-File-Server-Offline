6117cbd6141bfd1fbd0930771b03a8a0835fe017
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-11 22:44:31 +0600

    Deleted unnecessary class Main.

--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-	public static void main(String[] args) {
-		HtmlFileExplorer htmlFileExplorer = new HtmlFileExplorer();
-
-		System.out.println(htmlFileExplorer.getHtmlString("root"));
-	}
-}
