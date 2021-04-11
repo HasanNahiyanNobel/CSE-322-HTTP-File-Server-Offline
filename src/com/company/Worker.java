514e90f15cfa97056d201499480d0ea2e1c1cc1f
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-11 18:33:47 +0600

    Works better after flushing outputstreams instead of closing.

--- src/com/company/Worker.java
+++ src/com/company/Worker.java
@@ -1,46 +1,71 @@
 package com.company;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Worker extends Thread {
 	Socket socket;
+	int port;
+	String rootDirectoryPath;
 	boolean doWeNeedToScanHttpHeaders = false;
 
-	public Worker (Socket socket) {
+	public Worker (Socket socket, int port, String rootDirectoryPath) {
 		this.socket = socket;
+		this.port = port;
+		this.rootDirectoryPath = rootDirectoryPath;
 	}
 
 	public void run() {
 		try {
-			ObjectOutputStream objectOutputStream = new ObjectOutputStream(this.socket.getOutputStream());
-			PrintWriter printWriter = new PrintWriter(this.socket.getOutputStream());
-
+			//String localHostIP = serverSocket.getInetAddress().getLocalHost().toString().split("/")[1];
 			while (true) {
-				Scanner scanner = new Scanner(socket.getInputStream()); // From https://stackoverflow.com/a/35446009
+				String method = "";
+				String path = "";
+				String httpVersion = "";
+				String header = "";
 
-				if (scanner.hasNext()) {
-					String method = scanner.next();
-					String path = scanner.next();
-					path = path.substring(1); // Remove the slash at the beginning of the path
-					String httpVersion = scanner.next();
-					String header = "";
-					while (scanner.hasNext() && doWeNeedToScanHttpHeaders) {
-						header += scanner.next() + "\n";
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				String[] startLineContents = bufferedReader.readLine().split(" ");
+				method = startLineContents[0];
+				path = startLineContents[1].substring(1); // Substring taken to remove the prefix slash from path.
+				httpVersion = startLineContents[2];
+
+				if (doWeNeedToScanHttpHeaders) {
+					for (String headerLine=bufferedReader.readLine(); headerLine!=null && headerLine.length()>0; headerLine=bufferedReader.readLine()) {
+						header += headerLine;
 					}
-
-					String response = new HttpResponse("root").getResponse(path); // TODO: Change the root string to variable.
-					System.out.println(response);
-					//printWriter.write(response);
-					//printWriter.flush();
 				}
 
-				Thread.sleep(1000);
+				String httpResponse = new HttpResponse(rootDirectoryPath).getResponse(path);
+				File file = new File(rootDirectoryPath + "/" + path);
+
+				if (!file.exists() || file.isDirectory()) {
+					// Write http headers and html page
+					PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+					printWriter.print(httpResponse);
+					printWriter.flush();
+				}
+				else {
+					// Write http headers
+					PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+					printWriter.print(httpResponse);
+					System.out.println(httpResponse);
+					// Write the file
+					DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+					FileInputStream fileInputStream = new FileInputStream(file);
+					byte[] buffer = new byte[1024];
+
+					while (fileInputStream.read(buffer) > 0) {
+						dataOutputStream.write(buffer);
+					}
+
+					printWriter.flush();
+					dataOutputStream.flush();
+					fileInputStream.close();
+				}
 			}
-		} catch (IOException | InterruptedException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
