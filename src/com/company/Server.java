ce35fbd5279ca9e3af47b6807ee76094e15800bb
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-12 00:43:51 +0600

    Nietzsche wins, everything seems pointless.

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,8 +16,13 @@ public class Server {
 			Socket socket = serverSocket.accept();
 			System.out.println("Connection established.");
 
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+			DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+			DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
 			// Open thread
-			Thread worker = new ServerWorker(socket, PORT, ROOT_DIRECTORY_PATH);
+			Thread worker = new ServerWorker(socket, PORT, ROOT_DIRECTORY_PATH, bufferedReader, printWriter, dataInputStream, dataOutputStream);
 			worker.start();
 			numberOfThreads++;
 			System.out.println("Opened thread #" + numberOfThreads + "\n");
--- src/com/company/ServerWorker.java
+++ src/com/company/ServerWorker.java
@@ -7,12 +7,20 @@ public class ServerWorker extends Thread {
 	Socket socket;
 	int port;
 	String rootDirectoryPath;
+	BufferedReader bufferedReader;
+	PrintWriter printWriter;
+	DataInputStream dataInputStream;
+	DataOutputStream dataOutputStream;
 	final String LOG_FILE = "log.txt";
 
-	public ServerWorker (Socket socket, int port, String rootDirectoryPath) {
+	public ServerWorker (Socket socket, int port, String rootDirectoryPath, BufferedReader bufferedReader, PrintWriter printWriter, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
 		this.socket = socket;
 		this.port = port;
 		this.rootDirectoryPath = rootDirectoryPath;
+		this.bufferedReader = bufferedReader;
+		this.printWriter = printWriter;
+		this.dataInputStream = dataInputStream;
+		this.dataOutputStream = dataOutputStream;
 	}
 
 	public void run() {
@@ -23,7 +31,7 @@ public class ServerWorker extends Thread {
 				StringBuilder headers = new StringBuilder();
 				String request = "";
 
-				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				//BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
 				String startLine = bufferedReader.readLine();
 
@@ -57,18 +65,18 @@ public class ServerWorker extends Thread {
 
 				if (!requestedFile.exists() || requestedFile.isDirectory()) {
 					// Write http headers and html page
-					PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+					//PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
 					printWriter.print(httpResponse);
 					printWriter.flush();
 				}
 				else {
 					// Write http headers
-					PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+					//PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
 					printWriter.print(httpResponse);
 					printWriter.flush();
 
 					// Write the file
-					DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+					//DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 					FileInputStream fileInputStream = new FileInputStream(requestedFile);
 					byte[] buffer = new byte[1024];
 					while (fileInputStream.read(buffer) > 0) dataOutputStream.write(buffer);
@@ -86,7 +94,7 @@ public class ServerWorker extends Thread {
 		System.out.println("Upload request came, file name: " + fileName);
 		try {
 			// Read the file
-			DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+			//DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 			FileOutputStream fileOutputStream = new FileOutputStream(rootDirectoryPath+"/"+fileName);
 			byte[] buffer = new byte[1024];
 			while (dataInputStream.available() > 0) {
