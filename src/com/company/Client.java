53575abade4c0687253deacc18ac4206c019ce4a
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-11 21:10:05 +0600

    Deleted redundant class.

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -5,7 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Client1 {
+public class Client {
 	public static void main (String[] args) throws IOException, ClassNotFoundException {
 		Socket socket = new Socket("localhost", 8080);
 		System.out.println("Connection established");
