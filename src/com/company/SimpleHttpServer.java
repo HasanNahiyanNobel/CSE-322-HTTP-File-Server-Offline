b974a29a522fd03b7df87856e56b9338ef07143b
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-11 12:56:02 +0600

    Overlooking the wonder-bug for now. Implemented SimpleHttpServer.

--- src/com/company/SimpleHttpServer.java
+++ src/com/company/SimpleHttpServer.java
@@ -6,9 +6,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
-
-import static java.lang.System.exit;
 
 /**
  * Concept from: http://www.java2s.com/Code/Java/Network-Protocol/AverysimpleWebserverWhenitreceivesaHTTPrequestitsendstherequestbackasthereply.htm
@@ -17,6 +14,7 @@ public class SimpleHttpServer {
 	public static void main (String[] args) throws IOException {
 		int port = 8080;
 		boolean doWeNeedToScanHttpHeaders = false;
+
 		try {
 			ServerSocket serverSocket = new ServerSocket(port);
 
@@ -25,39 +23,31 @@ public class SimpleHttpServer {
 				Socket socket = serverSocket.accept();
 				System.out.println("Request received.");
 
-				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				for (String aLine=bufferedReader.readLine(); aLine!=null; aLine=bufferedReader.readLine()) {
-					System.out.println(aLine);
-				}
-				exit(0);
-
 				String method = "";
 				String path = "";
 				String httpVersion = "";
 				String header = "";
 
-				Scanner scanner = new Scanner(socket.getInputStream());
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-				if (scanner.hasNext()) {
-					method = scanner.next();
-					path = scanner.next();
-					path = path.substring(1); // Remove the slash at the beginning of the path
-					httpVersion = scanner.next();
-					while (scanner.hasNext() && doWeNeedToScanHttpHeaders) {
-						header += scanner.next() + "\n";
+				String[] startLineContents = bufferedReader.readLine().split(" ");
+				method = startLineContents[0];
+				path = startLineContents[1].substring(1); // Substring taken to remove the prefix slash from path.
+				httpVersion = startLineContents[2];
+
+				System.out.println(path);
+
+				if (doWeNeedToScanHttpHeaders) {
+					for (String headerLine=bufferedReader.readLine(); headerLine!=null && headerLine.length()>0; headerLine=bufferedReader.readLine()) {
+						header += headerLine;
 					}
 				}
-				System.out.println(path);
-				exit(0);
 
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintWriter out = new PrintWriter(socket.getOutputStream());
-
 				String httpResponse = new HttpResponse().getResponse(path);
 				out.print(httpResponse);
 
 				out.close();
-				in.close();
 				socket.close();
 			}
 		}
